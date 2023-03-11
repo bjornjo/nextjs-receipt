b6eb8c4ae01886910dbbef58ae1d933eceb4747c
Bjørn Johannessen <lesmooth.design@gmail.com>
2023-03-11 16:54:39 +0100

    Added all barcodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,21 @@ import bwipjs from 'bwip-js';
 import { Receipt } from '@/utils/types/receipt';
 import logo from "../../public/zeipt.svg";
 import regnskogfondet from "../../public/regnskogfondet.png";
+import QRCode from "react-qr-code";
+import { describe } from 'node:test';
 
 const Home = (props: any) => {
-  let receipt = props.receipt[0] as Receipt
-  let discounts = props.discounts as any
+  let receipt = props.receipt[0] as Receipt,
+    discounts = props.discounts as any,
+    bar_code = props.bar_code as any,
+    tickets = props.tickets as any
+
 
   return (
     <div className="wrapper">
       <div className="receipt-wrapper">
         <div className="receipt">
+
 
           {/* Merchant logo */}
           <div className="receipt-header">
@@ -318,17 +324,242 @@ const Home = (props: any) => {
             </div>
           }
 
-          <div className="box" style={{ border: 'none', background: 'none' }}>
+          {/* Regnskogfondet */}
+          <div className="box" style={{ background: 'none' }}>
             <div className="merchant-logo" style={{ "backgroundImage": "url(" + regnskogfondet.src + ")" }}>
             </div>
-            <h5 style={{ textAlign: "center" }}>No trees were harmed with this receipt</h5>
+            <h5>No trees were harmed with this receipt</h5>
           </div>
 
-          <img style={{ width: "100%" }} src={discounts[0].url} alt="Barcode" />
+          {/* Return / bar code */}
+          <div className="box">
+
+            {receipt.extra_receipt_view?.return_policy?.policy_description &&
+              <div style={{ marginBottom: 10 }}>
+                <h5>Return policy</h5>
+                <p>{receipt.extra_receipt_view?.return_policy?.policy_description}</p>
+              </div>
+            }
+            {/* Policy end date */}
+            {receipt.extra_receipt_view?.return_policy?.policy_end_date &&
+              <div style={{ marginBottom: 10 }}>
+                <h5>Last day of return:
+                </h5>
+                <p style={{ fontSize: '12px' }}>
+
+                  {new Date(
+                    receipt.extra_receipt_view?.return_policy?.policy_end_date
+                  ).toLocaleDateString("no-NO", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                  })}</p>
+              </div>
+            }
+            {receipt.extra_receipt_view?.bar_code?.value && receipt.extra_receipt_view.bar_code.encoding != "qr" ?
+              <div>
+                <img style={{ width: "100%" }} src={bar_code} alt="Barcode" />
+                <p className="amount" style={{ textAlign: 'center' }}>{receipt.extra_receipt_view?.bar_code.display_value && receipt.extra_receipt_view?.bar_code.display_value}</p>
+              </div>
+              : receipt.extra_receipt_view?.bar_code?.value && receipt.extra_receipt_view.bar_code.encoding == "qr" ?
+                <div>
+                  <QRCode style={{
+                    height: 100,
+                    width: 100
+                  }} value={bar_code.value} />
+                  <p className="amount" style={{ textAlign: 'center' }}>{receipt.extra_receipt_view?.bar_code.display_value && receipt.extra_receipt_view?.bar_code.display_value}</p>
+                </div>
+                : null
+            }
+          </div>
+
+          {/* Discounts */}
+          {receipt.extended_receipt_logic?.discounts && receipt.extended_receipt_logic?.discounts.map((discount, index) => {
+            return (
+              <div className="box" key={"discounts" + index}>
+
+                <div style={{ marginBottom: 10 }}>
+                  <h5>Discount on article:</h5>
+
+                  {discount.art_numbers?.map((discount_art_nr, index) => {
+                    return <p key={"discount_art_nr" + index}>{discount_art_nr}</p>
+                  })}
+
+                </div>
+
+                <div style={{ marginBottom: 10 }}>
+                  <h5>Discount amount:</h5>
+                  <p style={{ fontSize: '12px' }}>{discount.amount}</p>
+                </div>
+
+                <div style={{ marginBottom: 10 }}>
+                  <h5>Discount percentage:</h5>
+                  <p style={{ fontSize: '12px' }}>{discount.percentage}</p>
+                </div>
+
+                {discount.expiration_date &&
+                  <div style={{ marginBottom: 10 }}>
+                    <h5>Expiration date:</h5>
+                    <p style={{ fontSize: '12px' }}>
+
+                      {new Date(
+                        discount.expiration_date
+                      ).toLocaleDateString("no-NO", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                        hour: "numeric",
+                        minute: "numeric",
+                      })}
+                    </p>
+                  </div>
+                }
+
+                {/* Bar / QR code */}
+                <div>
+                  {discount.bar_code?.value && discount.bar_code.encoding != "qr" ?
+                    <div>
+                      <img key={"discount-" + index} style={{ width: "100%" }} src={discounts[index].url} alt="Barcode" />
+                      <p className="amount" style={{ textAlign: 'center' }}>{discount.bar_code.display_value && discount.bar_code.display_value}</p>
+                    </div>
+                    : discount.bar_code?.value && discount.bar_code.encoding == "qr" ?
+                      <div>
+                        <QRCode style={{
+                          height: 100,
+                          width: 100
+                        }} value={discount.bar_code.value} />
+                        <p className="amount" style={{ textAlign: 'center' }}>{discount.bar_code.display_value && discount.bar_code.display_value}</p>
+                      </div>
+                      : null
+                  }
+                </div>
+
+              </div>)
+          })}
+
+          {/* Tickets */}
+          {receipt.articles?.map((article, index) => {
+            if (article.bar_codes?.length && article.bar_codes?.length > 0) return (
+              <div className='box' key={"article-ticket" + index}>
+
+
+                {article.art_name && <p style={{ marginBottom: 10 }}>{article.art_name}</p>}
+
+                {/* Specified info */}
+                {article.specified &&
+                  <>
+                    {article.specified.floor_number &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Floor number
+                        </h5>
+                        <p>{article.specified.floor_number}</p>
+                      </div>
+                    }
+                    {article.specified.room_number &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Room number
+                        </h5>
+                        <p>{article.specified.room_number}</p>
+                      </div>
+                    }
+                    {article.specified.entrance &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Entrance
+                        </h5>
+                        <p>{article.specified.entrance}</p>
+                      </div>
+                    }
+                    {article.specified.row_number &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Row number
+                        </h5>
+                        <p>{article.specified.row_number}</p>
+                      </div>
+                    }
+                    {article.specified.seat_number &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Seat number
+                        </h5>
+                        <p>{article.specified.seat_number}</p>
+                      </div>
+                    }
+                    {article.specified.event_start &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Event start
+                        </h5>
+                        <p>{
+                          new Date(
+                            article.specified.event_start.substring(0, 19)
+                          ).toLocaleDateString(receipt.merchant.merchant_country_code, { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" })
+                        }
+                        </p>
+                      </div>
+                    }
+                    {article.specified.event_ending &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Event ending
+                        </h5>
+                        <p>
+                          {
+                            new Date(
+                              article.specified.event_ending.substring(0, 19)
+                            ).toLocaleDateString(receipt.merchant.merchant_country_code, { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" })
+                          }
+                        </p>
+                      </div>
+                    }
+                    {article.specified.expiration_date &&
+                      <div style={{ marginBottom: 10 }}>
+                        <h5>
+                          Expiration date
+                        </h5>
+
+                        <p>{
+                          new Date(
+                            article.specified.expiration_date.substring(0, 19)
+                          ).toLocaleDateString(receipt.merchant.merchant_country_code, { year: "numeric", month: "numeric", day: "numeric", hour: "numeric", minute: "numeric", second: "numeric" })
+
+                        }</p>
+                      </div>
+                    }
+                  </>
+                }
+                {/* Bar / QR code */}
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                  {article.bar_codes[0].encoding != "qr" ?
+                    <div>
+                      <img key={"discount-" + index} style={{ width: "100%" }} src={tickets[index].url} alt="Barcode" />
+                      <p className="amount" style={{ textAlign: 'center' }}>{article.bar_codes[0].display_value && article.bar_codes[0].display_value}</p>
+                    </div>
+
+                    : article.bar_codes[0].encoding == "qr" ?
+                      <div>
+                        <QRCode style={{
+                          height: 100,
+                          width: 100
+                        }} value={article.bar_codes[0].value} />
+                        <p className="amount" style={{ textAlign: 'center' }}>{article.bar_codes[0].display_value && article.bar_codes[0].display_value}</p>
+                      </div>
+                      :
+                      null
+                  }
+                </div>
+              </div>
+            )
+          })}
 
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 export default Home;
@@ -369,13 +600,29 @@ export const getServerSideProps: GetServerSideProps = async () => {
       console.log('Can\'t fetch receipt;', err);
     })
 
+  /* Receipt bar code */
+  let bar_code: any;
+  if (receipt[0].extra_receipt_view.bar_code.value && receipt[0].extra_receipt_view.bar_code.encoding != "qr") {
+    let options = {
+      bcid: receipt[0].extra_receipt_view.bar_code.encoding == "code_39" ? "code39"
+        : receipt[0].extra_receipt_view.bar_code.encoding == "ean_13" ? "ean13"
+          : receipt[0].extra_receipt_view.bar_code.encoding == "ean_8" ? "ean8"
+            : receipt[0].extra_receipt_view.bar_code.encoding == "interleaved_2_of_5" ? "itf14"
+              : "code128",
+      text: receipt[0].extra_receipt_view.bar_code.value,
+      scale: 3,
+      height: 10,
+      includetext: false,
+    };
+    let buffer = await bwipjs.toBuffer(options);
+    let imagePath = `data:image/png;base64,${buffer.toString('base64')}`;
+    bar_code = imagePath
+  }
   /* Discount bar codes */
   let discounts: any = [];
   if (receipt[0].extended_receipt_logic?.discounts) {
     for (const discount of receipt[0].extended_receipt_logic?.discounts) {
-
       if (discount.bar_code?.value && discount.bar_code.encoding != "qr") {
-
         let options = {
           bcid: discount.bar_code.encoding == "code_39" ? "code39"
             : discount.bar_code.encoding == "ean_13" ? "ean13"
@@ -384,20 +631,49 @@ export const getServerSideProps: GetServerSideProps = async () => {
                   : "code128",
           text: discount.bar_code.value,
           scale: 3,
-          height: 20,
-          includetext: true,
+          height: 10,
+          includetext: false,
         };
         let buffer = await bwipjs.toBuffer(options);
         let imagePath = `data:image/png;base64,${buffer.toString('base64')}`;
         discounts.push({ "url": imagePath })
+      } else {
+        discounts.push({ "url": "" })
       }
     }
   }
-
+  /* Ticket bar codes */
+  let tickets: any = [];
+  if (receipt[0].extended_receipt_logic?.discounts) {
+    for (const ticket of receipt[0].articles) {
+      if (ticket.bar_codes?.length && ticket.bar_codes?.length > 0) {
+        if (ticket.bar_codes[0]?.value && ticket.bar_codes[0].encoding != "qr") {
+          let options = {
+            bcid: ticket.bar_codes[0].encoding == "code_39" ? "code39"
+              : ticket.bar_codes[0].encoding == "ean_13" ? "ean13"
+                : ticket.bar_codes[0].encoding == "ean_8" ? "ean8"
+                  : ticket.bar_codes[0].encoding == "interleaved_2_of_5" ? "itf14"
+                    : "code128",
+            text: ticket.bar_codes[0].value,
+            scale: 3,
+            height: 10,
+            includetext: false,
+          };
+          let buffer = await bwipjs.toBuffer(options);
+          let imagePath = `data:image/png;base64,${buffer.toString('base64')}`;
+          tickets.push({ "url": imagePath })
+        } else {
+          tickets.push({ "url": "" })
+        }
+      }
+    }
+  }
   return {
     props: {
+      receipt,
       discounts,
-      receipt
+      bar_code,
+      tickets
     },
   };
 };
