97166a55ea86b538b927e1877b79cf4d1b1291d9
Bjørn Johannessen <lesmooth.design@gmail.com>
2023-04-15 18:29:06 +0200

    fix api

--- src/pages/api/pdf.ts
+++ src/pages/api/pdf.ts
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
 import { NextApiRequest, NextApiResponse } from 'next';
+import chromium from 'chrome-aws-lambda';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -11,14 +12,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       queryParams[key] = value;
     });
 
-    const browser = await puppeteer.launch();
+    const browser = await chromium.puppeteer.launch({
+    args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: true,
+    ignoreHTTPSErrors: true,
+  })
     const page = await browser.newPage();
 
     await page.goto(url as string, {
       waitUntil: 'networkidle2',
     });
 
-    const pdf = await page.pdf({ format: 'A4' });
+    const pdf = await page.pdf({ format: 'a4' });
 
     fs.writeFileSync('page.pdf', pdf);
 
